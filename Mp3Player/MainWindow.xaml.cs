bb5848e163cdbfd1e102f99b9c0871e000a75ad2
Damian Wnukowski <Damian.Wnukowski97@gmail.com>
2018-02-17 19:06:09 +0100

    Most of the view is done, some logic added (you can add files to playlist but playing hasn't been implemented at all yet).

--- Mp3Player/MainWindow.xaml.cs
+++ Mp3Player/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,17 +14,39 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Media;
+using Microsoft.Win32;
+using WMPLib;
 
 namespace Mp3Player
 {
+	
+	
 	/// <summary>
 	/// Logika interakcji dla klasy MainWindow.xaml
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		public ObservableCollection<Mp3File> Mp3Files { get; set; }
+
 		public MainWindow()
 		{
 			InitializeComponent();
+			Mp3Files = new ObservableCollection<Mp3File>();
+		}
+
+		private void OpenButton_OnClick(object sender, RoutedEventArgs e)
+		{
+			OpenFileDialog openFileDialog = new OpenFileDialog();
+			openFileDialog.Multiselect = true;
+			openFileDialog.Filter = "mp3 files (*.mp3)|*.mp3";
+			openFileDialog.ShowDialog();
+
+			foreach (var mp3Path in openFileDialog.FileNames)
+			{
+				Mp3Files.Add(new Mp3File(mp3Path));
+			}
+
 		}
 	}
 }
---
+++ Mp3Player/mp3File.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TagLib;
+
+namespace Mp3Player
+{
+	public class Mp3File
+	{
+		public string Path { get; }
+		public string Duration { get; }
+		public string Title { get; }
+		public string FullTitle //Title that has author
+			=> Author + " - " +  Title;
+
+		public string Author { get; }
+
+		public Mp3File(string path)
+		{
+			Path = path;
+			File f = File.Create(path);
+			Duration = String.Format("{0}:{1:00}", f.Properties.Duration.Minutes, f.Properties.Duration.Seconds);
+			Title = f.Tag.Title;
+			Author = f.Tag.FirstPerformer;
+		}
+	}
+}
