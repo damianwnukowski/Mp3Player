270749dddf974ef261229a6fbb202fbb09ca09ef
Damian Wnukowski <Damian.Wnukowski97@gmail.com>
2018-02-19 11:11:44 +0100

    Art covers working, some other changes before I will change mp3 player logic from mp3file class to mainwindow class.

--- Mp3Player/MainWindow.xaml.cs
+++ Mp3Player/MainWindow.xaml.cs
@@ -28,11 +28,16 @@ namespace Mp3Player
 	public partial class MainWindow : Window
 	{
 		public ObservableCollection<Mp3File> Mp3Files { get; set; }
+		public BitmapImage DefaultArtwork { get; }
 
 		public MainWindow()
 		{
 			InitializeComponent();
 			Mp3Files = new ObservableCollection<Mp3File>();
+			DefaultArtwork = new BitmapImage();
+			DefaultArtwork.BeginInit();
+			DefaultArtwork.UriSource = new Uri("Resource Images/DefaultArtwork.png", UriKind.Relative);
+			DefaultArtwork.EndInit();
 		}
 
 		private void OpenButton_OnClick(object sender, RoutedEventArgs e)
@@ -42,18 +47,25 @@ namespace Mp3Player
 			openFileDialog.Filter = "mp3 files (*.mp3)|*.mp3";
 			openFileDialog.ShowDialog();
 
-			foreach (var mp3Path in openFileDialog.FileNames)
+
+			if (openFileDialog.FileNames.Length > 0)
 			{
-				Mp3Files.Add(new Mp3File(mp3Path));
+				Mp3File firstMp3 = new Mp3File(openFileDialog.FileNames[0]);
+				Mp3Files.Add(firstMp3);
+				for (int i = 1; i < openFileDialog.FileNames.Length; i++)
+				{
+					Mp3Files.Add(new Mp3File(openFileDialog.FileNames[i]));
+				}
+				firstMp3.Play(this);
 			}
-			Mp3Files.First().Play(this);
+
 		}
 
-		private void PlayButton_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+		private void ResumeButton_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
 		{
 			if (Mp3File.CurrentPlaying == null)
 				return;
-			Mp3File.CurrentPlaying.Play(this);
+			Mp3File.CurrentPlaying.Resume(this);
 		}
 
 		private void PauseButton_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -62,5 +74,7 @@ namespace Mp3Player
 				return;
 			Mp3File.CurrentPlaying.Pause(this);
 		}
+
+		
 	}
 }
--- Mp3Player/mp3File.cs
+++ Mp3Player/mp3File.cs
@@ -1,60 +1,90 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
-using Mp3Player.Annotations;
-using TagLib;
+using System.Windows.Media.Imaging;
 using WMPLib;
+using File = TagLib.File;
+
 
 namespace Mp3Player
 {
 	public class Mp3File
 	{
 		public static Mp3File CurrentPlaying { get; set; }
-		public static WindowsMediaPlayer _player = new WindowsMediaPlayer();
+		public static WindowsMediaPlayer Player = new WindowsMediaPlayer();
 
-		public string Path { get; }
+		public double MaxPosition;
+		public string FilePath { get; }
 		public string Duration { get; }
 		public string Title { get; }
-		public string FullTitle //Title that has author
-			=> Author + " - " +  Title;
-
+		public BitmapImage AlbumCover { get; }
 		public string Author { get; }
 
-		static Mp3File()
+		public string FullTitle //Title that begins with author
 		{
+			get
+			{
+				if (Title == null || Author == null)
+				{
+					return Path.GetFileName(FilePath);
+				}
 
+				return Author + " - " + Title;
+			}
 		}
+
+		
+
 
 		public Mp3File(string path)
 		{
-			Path = path;
+			FilePath = path;
 			File f = File.Create(path);
 			Duration = String.Format("{0}:{1:00}", f.Properties.Duration.Minutes, f.Properties.Duration.Seconds);
 			Title = f.Tag.Title;
 			Author = f.Tag.FirstPerformer;
+
+			//retrieving album cover as byte array and convering it into bitmapimage
+			if (f.Tag.Pictures.Length != 0)
+			{
+				var img = f.Tag.Pictures[0].Data.Data;
+				AlbumCover = new BitmapImage();
+				AlbumCover.BeginInit();
+				AlbumCover.StreamSource = new MemoryStream(img);
+				AlbumCover.EndInit();
+			}
 		}
 
-		public void Play(MainWindow resource)
+		public void Play(MainWindow w)
 		{
 			CurrentPlaying = this;
-			_player.URL = Path;
-			_player.controls.play();
-			resource.TitleTextBox.Text = CurrentPlaying.FullTitle;
-			resource.PlayButton.Visibility = Visibility.Collapsed;
-			resource.PauseButton.Visibility = Visibility.Visible;
+
+			if (AlbumCover != null)
+				w.AlbumCover.Source = AlbumCover;
+			else
+				w.AlbumCover.Source = w.DefaultArtwork;
+
+			Player.URL = FilePath;
+			Player.controls.play();
+			w.TitleTextBox.Text = CurrentPlaying.FullTitle;
+			w.ResumeButton.Visibility = Visibility.Collapsed;
+			w.PauseButton.Visibility = Visibility.Visible;
 
 		}
 
-		public void Pause(MainWindow resource)
+		public void Pause(MainWindow w)
 		{
-			resource.PlayButton.Visibility = Visibility.Visible;
-			resource.PauseButton.Visibility = Visibility.Collapsed;
-			_player.controls.pause();
+			w.ResumeButton.Visibility = Visibility.Visible;
+			w.PauseButton.Visibility = Visibility.Collapsed;
+			Player.controls.pause();
 		}
+
+		public void Resume(MainWindow w)
+		{
+			w.ResumeButton.Visibility = Visibility.Collapsed;
+			w.PauseButton.Visibility = Visibility.Visible;
+			Player.controls.play();
+		}
+
 	}
 }
